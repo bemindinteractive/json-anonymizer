d376caa8df11c326d82acb2d98ab73ae3db1f044
echidna <sparagnufolo@gmail.com>
2015-04-26 16:54:15 +0200

    splitted core functionality from the cordova specific use-case

--- anonymize.js
+++ anonymize.js
@@ -4,56 +4,28 @@
  * @return {string}        Anonymized JSON string
  * @author Lorenzo De Santis <lorenzo@bemind.me>
  */
-var anonymize = function(jStr) {
+var anonymize = function(jStr, keywords) {
     var res = jStr;
 
-    //properties and stringified boolean values
-    var strings = [
-        "photos",
-        "id",
-        "phoneNumbers",
-        "value",
-        "type",
-        "emails",
-        "displayName",
-        "name",
-        "givenName",
-        "formatted",
-        "middleName",
-        "familyName",
-        "honorificPrefix",
-        "honorificSuffix",
-        "nickname",
-        "pref",
-        "rawId",
-        "addresses",
-        "ims",
-        "organizations",
-        "title",
-        "department",
-        "birthday",
-        "note",
-        "categories",
-        "urls",
-        "true",
-        "false"
-    ];
+    keywords = keywords || [];
 
-	//pure values
+    //pure values
     var natives = [
         "true",
         "false",
-        "null"
+        "null",
+        "undefined",
+        "NaN"
     ];
 
     //properties and stringified boolean values
-    for (var i = strings.length - 1; i >= 0; i--) {
-        res = res.replace(new RegExp('\"' + strings[i] + '\"', 'g'), '"§' + i + '§"');
+    for (var i = keywords.length - 1; i >= 0; i--) {
+        res = res.replace(new RegExp('\"' + keywords[i] + '\"', 'g'), '"§' + i + '§"');
     }
 
     //pure nullable and boolean values
     for (var j = natives.length - 1; j >= 0; j--) {
-        res = res.replace(new RegExp('\:' + natives[j], 'g'), '"§' + (j + strings.length) + '§"');
+        res = res.replace(new RegExp('\:' + natives[j], 'g'), '"§' + (j + keywords.length) + '§"');
     }
 
     //alphabetic values
@@ -61,12 +33,12 @@ var anonymize = function(jStr) {
 
     //revert nullable and boolean values
     for (j = natives.length - 1; j >= 0; j--) {
-        res = res.replace(new RegExp('\"§' + (j + strings.length) + '§\"', 'g'), ':' + natives[j]);
+        res = res.replace(new RegExp('\"§' + (j + keywords.length) + '§\"', 'g'), ':' + natives[j]);
     }
 
     //revert properties and stringified boolean values
-    for (i = strings.length - 1; i >= 0; i--) {
-        res = res.replace(new RegExp('\"§' + i + '§\"', 'g'), '"' + strings[i] + '"');
+    for (i = keywords.length - 1; i >= 0; i--) {
+        res = res.replace(new RegExp('\"§' + i + '§\"', 'g'), '"' + keywords[i] + '"');
     }
 
     //numeric values
